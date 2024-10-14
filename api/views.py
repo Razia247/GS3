1e5b191930e31f9a35e562382230f7445cb2f1a5
Razia247 <razia63483@gmail.com>
2024-10-14 20:02:23 +0500

    GS3

---
+++ api/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+import io
+from rest_framework.parsers import JSONParser
+from api.models import Student
+from api.serializers import StudentSerializer
+from rest_framework.renderers import JSONRenderer
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+# Create your views here.
+@csrf_exempt
+def student_api(request):
+    if request.method=='GET':
+        json_data=request.body
+        stream=io.BytesIO(json_data)
+        python_data=JSONParser().parse(stream)
+        id = python_data.get('id',None)
+        if id is not None:
+            stu=Student.objects.get(id=id)
+            serializer=StudentSerializer(stu)
+            json_data=JSONRenderer().render(serializer.data)
+            return HttpResponse(json_data,content_type='application/json')
+        
